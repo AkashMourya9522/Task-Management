6da4df60ea1cd16a62e7087c023e4f8ec1f925f4
Akash Mourya <akashmourya9522@gmail.com>
2025-03-05 17:21:11 +0530

    Added Update A Task, Get A Task, Get All Tasks

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -1,51 +1,126 @@
 import Task from "../models/task.model.js";
 
-
 export const createTask = async (req, res) => {
-    const { title, description } = req.body;
-    const userId = req.userId  
-    try {
-      const newTask = new Task({ title, description,userRef:userId });
-      const dbRes = await newTask.save();
-      console.log(dbRes);
-      
-      res.status(200).json(dbRes)
-    } catch (error) {      
-      return res.status(404).json({
-          msg:error,
-          success:false
-      })
-    }
+  const { title, description } = req.body;
+  const userId = req.userId;
+  try {
+    const newTask = new Task({ title, description, userRef: userId });
+    const dbRes = await newTask.save();
+    res.status(200).json(dbRes);
+  } catch (error) {
+    return res.status(404).json({
+      msg: error,
+      success: false,
+    });
   }
+};
 
-  export const deleteTask = async (req,res)=>{
-    const taskId = req.params.id;
-    try {
-    const task = await Task.findById(taskId)
-    if(!task){
+export const deleteTask = async (req, res) => {
+  const taskId = req.params.id;
+  try {
+    const task = await Task.findById(taskId);
+    if (!task) {
       return res.status(404).json({
-        msg:"Invalid Request"
-      })
-    }
-     else if(task.userRef == req.userId){
+        msg: "Invalid Request",
+      });
+    } else if (task.userRef == req.userId) {
       try {
-        await Task.findByIdAndDelete(taskId)
+        await Task.findByIdAndDelete(taskId);
         return res.status(200).json({
-          msg:"Task Deleted Successfully",
-          success:true
-        })
+          msg: "Task Deleted Successfully",
+          success: true,
+        });
       } catch (error) {
         return res.status(404).json({
-          msg:error,
-          success:false
-        })
+          msg: error,
+          success: false,
+        });
       }
     }
-    } catch (error) {
-      return res.status(404).json({
-        msg:`Error Occurred ${error}`,
-        success:false
-      })
-    }
-    
+  } catch (error) {
+    return res.status(404).json({
+      msg: `Error Occurred ${error}`,
+      success: false,
+    });
   }
+};
+
+export const updateTask = async (req, res) => {
+  const taskId = req.params.id;
+  if (!taskId) {
+    return res.status(404).json({
+      msg: "Provide Task Id",
+      success: false,
+    });
+  }
+  try {
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({
+        msg: "Task Not Found",
+        success: false,
+      });
+    } else {
+      const userId = task.userRef;
+      if (userId == req.userId) {
+        const { title, description } = req.body;
+        const dbRes = await Task.findByIdAndUpdate(
+          taskId,
+          { title, description },
+          { new: true }
+        );
+        return res.status(200).json({
+          msg: "Task Updated Successfully",
+          success: true,
+          data: dbRes,
+        });
+      }
+    }
+  } catch (error) {
+    return res.status(404).json({
+      msg: error,
+      success: false,
+    });
+  }
+};
+
+export const getTasks = async (req, res) => {
+  const userId = req.userId;
+  try {
+    const tasks = await Task.find({ userRef: userId });
+    return res.status(200).json({
+      data: tasks,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      msg: error,
+      success: false,
+    });
+  }
+};
+
+export const getTask = async (req, res) => {
+  const taskId = req.params.id;
+  if (!taskId) {
+    return res.status(404).json({
+      msg: "Task Not Found",
+      success: false,
+    });
+  }
+  try {
+    const task = await Task.findById(taskId);
+    if (task.userRef != req.userId) {
+      return res.status(404).json({
+        msg: "UnAuthorized",
+        success: false,
+      });
+    } else {
+      return res.status(200).json({
+        msg: "Success",
+        success: true,
+        data: task,
+      });
+    }
+  } catch (error) {}
+};
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 
-import { createTask, deleteTask } from "../controller/task.controller.js";
+import { createTask, deleteTask, getTask, getTasks, updateTask } from "../controller/task.controller.js";
 import verifyUser from "../miscellaneous/verifyUserMiddleware.js";
 const route = express.Router();
 
 route.post("/create",verifyUser ,createTask);
 route.delete("/delete/:id",verifyUser,deleteTask)
+route.put("/update/:id",verifyUser,updateTask)
+route.get("/getTasks",verifyUser,getTasks)
+route.get("/getTask/:id",verifyUser,getTask)
+
 
 export default route;
